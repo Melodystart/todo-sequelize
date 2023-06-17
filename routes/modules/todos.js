77199aa02cdbaa9db203a56ed18af152cf0667a4
Melodystart <start99start@gmail.com>
2023-06-17 16:13:38 +0800

    add relations between User and Todo models

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,59 +4,60 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-router.get('/:id',(req,res) => {
+router.get('/:id', (req, res) => {
+    const UserId = req.user.id
     const id = req.params.id
-    return Todo.findByPk(id)
-    .then (todo => res.render('detail', {todo: todo.toJSON()}))
-    .catch(error => console.log(error))
-  })
+    return Todo.findOne({ where: { id, UserId } })
+        .then(todo => res.render('detail', { todo: todo.toJSON() }))
+        .catch(error => console.log(error))
+})
 
 router.get('/new', (req, res) => {
-return res.render('new')
+    return res.render('new')
 })
 
 router.post('/', (req, res) => {
-const name = req.body.name
-return Todo.create({ name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    const name = req.body.name
+    return Todo.create({ name })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
 })
 
 router.get('/:id', (req, res) => {
-const id = req.params.id
-return Todo.findByPk(id)
-    .lean()
-    .then(todo => res.render('detail', {todo: todo.toJSON()}))
-    .catch(error => console.log(error))
+    const id = req.params.id
+    return Todo.findByPk(id)
+        .lean()
+        .then(todo => res.render('detail', { todo: todo.toJSON() }))
+        .catch(error => console.log(error))
 })
 
 router.get('/:id/edit', (req, res) => {
-const id = req.params.id
-return Todo.findByPk(id)
-    .lean()
-    .then(todo => res.render('edit', {todo: todo.toJSON()}))
-    .catch(error => console.log(error))
+    const id = req.params.id
+    return Todo.findByPk(id)
+        .lean()
+        .then(todo => res.render('edit', { todo: todo.toJSON() }))
+        .catch(error => console.log(error))
 })
 
 router.put('/:id', (req, res) => {
-const id = req.params.id
-const { name, isDone } = req.body
-return Todo.findByPk(id)
-    .then(todo => {
-    todo.name = name
-    todo.isDone = isDone === 'on'
-    return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
+    const id = req.params.id
+    const { name, isDone } = req.body
+    return Todo.findByPk(id)
+        .then(todo => {
+            todo.name = name
+            todo.isDone = isDone === 'on'
+            return todo.save()
+        })
+        .then(() => res.redirect(`/todos/${id}`))
+        .catch(error => console.log(error))
 })
 
 router.delete('/:id', (req, res) => {
-const id = req.params.id
-return Todo.findByPk(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    const id = req.params.id
+    return Todo.findByPk(id)
+        .then(todo => todo.remove())
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
 })
 
 module.exports = router
